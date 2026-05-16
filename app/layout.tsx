14c8e52664ba87a093d8cdc7065c7bb39dbb248e
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2026-05-16 12:58:22 +0600

    Feat: Initial Commit, Project setup done.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/sections/header";
 
-const geistSans = Geist({
+export const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+export const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="dark">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Navbar />
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-1">{children}</main>
+          {/* <Footer /> */}
+        </div>
+      </body>
     </html>
   );
 }
